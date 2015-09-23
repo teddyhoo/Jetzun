1f198ae092b99fdad211206aadf57d42a6a9e14e
teddyhoo <teddyhoo@hotmail.com>
2015-09-23 09:29:56 -0400

    Initial Commit

---
+++ Jetzun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jetzun
+//
+//  Created by Ted Hooban on 9/23/15.
+//  Copyright (c) 2015 Ted Hooban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
